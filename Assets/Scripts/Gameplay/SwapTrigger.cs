3239ad38f872ef0229c79d44e9b6fa360590592e
Kerem Haktan Kurt <kajuyerim@protonmail.com>
2026-05-24 15:16:58 +0300

    feat(polish): swap-trigger banner telegraph
    Recolors the trigger's sprite by swapped action while armed; greys it
    out once crossed. Audio sting deferred.
    
    Closes #41

--- Assets/Scripts/Gameplay/SwapTrigger.cs
+++ Assets/Scripts/Gameplay/SwapTrigger.cs
@@ -8,13 +8,26 @@ namespace JumpNowBro.Gameplay
     public class SwapTrigger : MonoBehaviour
     {
         static readonly List<SwapTrigger> active = new List<SwapTrigger>();
+        static Sprite quadSprite;
 
         [SerializeField] PlayerAction actionToSwap;
+        [SerializeField] bool showBanner = true;
+        [SerializeField] float armedAlpha = 0.55f;
+        [SerializeField] Color firedColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+        [SerializeField] int bannerSortingOrder = 1;
+
         bool fired;
         bool firedAtCheckpoint;
+        SpriteRenderer banner;
 
         void OnEnable() => active.Add(this);
         void OnDisable() => active.Remove(this);
+
+        void Start()
+        {
+            if (showBanner) AcquireBanner();
+            SetBannerArmed(!fired);
+        }
 
         // Checkpoint locks in which triggers have fired up to this point.
         public static void SaveCheckpointStates()
@@ -25,7 +38,11 @@ namespace JumpNowBro.Gameplay
         // Respawn: triggers crossed since the checkpoint re-arm; ones crossed before it stay consumed.
         public static void RestoreCheckpointStates()
         {
-            foreach (var t in active) t.fired = t.firedAtCheckpoint;
+            foreach (var t in active)
+            {
+                t.fired = t.firedAtCheckpoint;
+                t.SetBannerArmed(!t.fired);
+            }
         }
 
         void OnTriggerEnter2D(Collider2D other)
@@ -42,6 +59,70 @@ namespace JumpNowBro.Gameplay
 
             fired = true;
             store.Apply(ControlMap.WithSwap(store.Current, actionToSwap));
+            SetBannerArmed(false);
+        }
+
+        void AcquireBanner()
+        {
+            // Prefer a sprite the level author already placed on the trigger so we
+            // recolor that instead of stacking a second quad on top of it.
+            banner = GetComponentInChildren<SpriteRenderer>();
+            if (banner == null) BuildBanner();
+        }
+
+        void BuildBanner()
+        {
+            var col = GetComponent<Collider2D>();
+            var go = new GameObject("SwapBanner");
+            banner = go.AddComponent<SpriteRenderer>();
+            banner.sprite = QuadSprite();
+            banner.sortingOrder = bannerSortingOrder;
+
+            if (col is BoxCollider2D box)
+            {
+                go.transform.SetParent(transform, worldPositionStays: false);
+                go.transform.localPosition = box.offset;
+                go.transform.localScale = new Vector3(box.size.x, box.size.y, 1f);
+            }
+            else
+            {
+                // Non-box colliders: size from world bounds, parented in world space.
+                var b = col.bounds;
+                go.transform.SetParent(transform, worldPositionStays: true);
+                go.transform.position = new Vector3(b.center.x, b.center.y, transform.position.z);
+                go.transform.localScale = new Vector3(b.size.x, b.size.y, 1f);
+            }
+        }
+
+        void SetBannerArmed(bool armed)
+        {
+            if (banner == null) return;
+            banner.color = armed ? ColorFor(actionToSwap, armedAlpha) : firedColor;
+        }
+
+        static Color ColorFor(PlayerAction action, float alpha)
+        {
+            Color c = action switch
+            {
+                PlayerAction.MoveHorizontal => new Color(0.4f, 0.9f, 0.5f),
+                PlayerAction.Jump => new Color(1f, 0.7f, 0.2f),
+                PlayerAction.Dash => new Color(0.4f, 0.8f, 1f),
+                _ => Color.white
+            };
+            c.a = alpha;
+            return c;
+        }
+
+        static Sprite QuadSprite()
+        {
+            if (quadSprite == null)
+            {
+                var tex = Texture2D.whiteTexture;
+                quadSprite = Sprite.Create(
+                    tex, new Rect(0, 0, tex.width, tex.height),
+                    new Vector2(0.5f, 0.5f), tex.width);
+            }
+            return quadSprite;
         }
     }
 }
